e8146888926ab5faacb3f72e71a4e55509b7f1d3
Jason <numberthirteen4026@gmail.com>
2022-09-01 12:56:41 +0200

    Added a game loop using a thread

--- src/pl/jasonxiii/pong/GamePanel.java
+++ src/pl/jasonxiii/pong/GamePanel.java
@@ -5,21 +5,62 @@ import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable
 {
+	private final Thread thread;
+	private boolean isRunning;
+
 	public GamePanel()
 	{
 		setBackground(Constants.BACKGROUND_COLOR);
 		setPreferredSize(new Dimension(Constants.GAME_WIDTH, Constants.GAME_HEIGHT));
+
+		thread = new Thread(this);
+		isRunning = true;
+
+		thread.start();
 	}
 
 	@Override
 	public void run()
 	{
+		long previousFrame = System.currentTimeMillis();
+		long difference;
+		long delay;
+		int ms = 1000 / Constants.GAME_FPS;
 
+		while (isRunning)
+		{
+			update();
+			repaint();
+
+			difference = System.currentTimeMillis() - previousFrame;
+			delay = ms - difference;
+
+			if(delay < 0)
+			{
+				delay = 2;
+			}
+
+			try
+			{
+				Thread.sleep(delay);
+			}
+			catch (InterruptedException ie)
+			{
+				ie.printStackTrace();
+			}
+
+			previousFrame = System.currentTimeMillis();
+		}
 	}
 
 	@Override
 	protected void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
+	}
+
+	private void update()
+	{
+		System.out.println("Updating");
 	}
 }
