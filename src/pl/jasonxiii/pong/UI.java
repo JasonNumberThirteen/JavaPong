d1b9693f6fa451ae04f123226d55d62da5d1c02d
Jason <numberthirteen4026@gmail.com>
2022-09-02 14:03:13 +0200

    Added antialiasing for UI texts

--- src/pl/jasonxiii/pong/UI.java
+++ src/pl/jasonxiii/pong/UI.java
@@ -7,8 +7,11 @@ public class UI implements Drawable
 	@Override
 	public void draw(Graphics g)
 	{
+		Graphics2D g2D = (Graphics2D)g;
+
 		g.setColor(Color.WHITE);
 		g.setFont(uiFont());
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.drawString("0", (Constants.GAME_WIDTH >> 1) - 48, Constants.UI_FONT_SIZE);
 		g.drawString("0", (Constants.GAME_WIDTH >> 1) + 48, Constants.UI_FONT_SIZE);
 	}
