33a736c5078c34cb3238b3b4f6eba7d58c823d0e
Jason <numberthirteen4026@gmail.com>
2022-09-03 11:51:59 +0200

    Added player won text display when the game is over

--- src/pl/jasonxiii/pong/uitexts/PlayerWonText.java
+++ src/pl/jasonxiii/pong/uitexts/PlayerWonText.java
@@ -1,6 +1,7 @@
 package pl.jasonxiii.pong.uitexts;
 
 import pl.jasonxiii.pong.Constants;
+import pl.jasonxiii.pong.GameManager;
 
 import java.awt.*;
 
@@ -9,5 +10,11 @@ public class PlayerWonText extends UIText
 	public PlayerWonText()
 	{
 		super("PLAYER 1 WON!", new Point(Constants.GAME_WIDTH >> 1, Constants.GAME_HEIGHT >> 1), Color.ORANGE);
+	}
+
+	@Override
+	public boolean isVisible()
+	{
+		return GameManager.INSTANCE.isOver();
 	}
 }
