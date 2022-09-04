293c34cfaa1ec90045d08c784c2fc7f917d189d8
Jason <numberthirteen4026@gmail.com>
2022-09-04 12:07:43 +0200

    Optimised Paddle class

--- src/pl/jasonxiii/pong/gameobjects/Paddle.java
+++ src/pl/jasonxiii/pong/gameobjects/Paddle.java
@@ -1,7 +1,6 @@
 package pl.jasonxiii.pong.gameobjects;
 
 import pl.jasonxiii.pong.Constants;
-import pl.jasonxiii.pong.GameManager;
 import pl.jasonxiii.pong.PlayerInput;
 import pl.jasonxiii.pong.interfaces.Collidable;
 import pl.jasonxiii.pong.interfaces.Drawable;
@@ -26,16 +25,7 @@ public class Paddle extends GameObject implements Updatable, Drawable, Collidabl
 	@Override
 	public void update(double delta)
 	{
-		int movementStep = (int)(Constants.PADDLE_MOVEMENT_SPEED*delta);
-
-		if(input.isMovingUp())
-		{
-			position.y = Methods.clampInt(0, position.y - movementStep, Constants.GAME_HEIGHT - Constants.PADDLE_HEIGHT);
-		}
-		else if(input.isMovingDown())
-		{
-			position.y = Methods.clampInt(0, position.y + movementStep, Constants.GAME_HEIGHT - Constants.PADDLE_HEIGHT);
-		}
+		move(delta);
 	}
 
 	@Override
@@ -67,5 +57,29 @@ public class Paddle extends GameObject implements Updatable, Drawable, Collidabl
 	public Rectangle collisionBox()
 	{
 		return new Rectangle(position.x, position.y, Constants.PADDLE_WIDTH, Constants.PADDLE_HEIGHT);
+	}
+
+	private void move(double delta)
+	{
+		int movementStep = (int)(Constants.PADDLE_MOVEMENT_SPEED*delta);
+		int max = Constants.GAME_HEIGHT - Constants.PADDLE_HEIGHT;
+		int direction = movementDirection();
+		int offset = movementStep*direction;
+
+		position.y = Methods.clampInt(0, position.y + offset, max);
+	}
+
+	private int movementDirection()
+	{
+		if(input.isMovingUp())
+		{
+			return -1;
+		}
+		else if(input.isMovingDown())
+		{
+			return 1;
+		}
+
+		return 0;
 	}
 }
